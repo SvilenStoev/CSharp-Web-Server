7dea39f85d0b6ce2720d82ddf627d1806e4e7cd2
Svilen Stoev <svilen.d.stoev@abv.bg>
2021-06-16 16:44:40 +0300

    Created web server - initial settings and connection

---
+++ MyHTTPServer/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace MyHTTPServer
+{
+    class StartUp
+    {
+        public static async Task Main(string[] args)
+        {
+            string ipAddress = "127.0.0.1";
+
+            var address = IPAddress.Parse(ipAddress);
+            int port = 6969;
+
+            var serverListener = new TcpListener(address, port);
+
+            serverListener.Start();
+
+            Console.WriteLine($"Server started on port {port}...");
+            Console.WriteLine("Waiting for client requests...");
+
+            var connection = await serverListener.AcceptTcpClientAsync();
+
+              
+
+
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CSharp-Web-Server
+
+My simple C# web server created for educational purposes.
