fb8591cdb97af066f6bd1b62413dcbb8c7d65e32
Svilen Stoev <svilen.d.stoev@abv.bg>
2021-06-17 18:50:58 +0300

    Adding function for reading the request from the client

--- MyHTTPServer/StartUp.cs
+++ MyHTTPServer/StartUp.cs
@@ -29,10 +29,36 @@ namespace MyHTTPServer
 
                 var networkStream = connection.GetStream();
 
-                var responseBody = "<h1>Здрасти!</h1>";
+                var bufferLenght = 1024;
+                var buffer = new byte[bufferLenght];
+
+                var totalBytesRead = 0;
+
+                var requestBuilder = new StringBuilder();
+
+                do
+                {
+                    var bytesRead = await networkStream.ReadAsync(buffer, 0, bufferLenght);
+
+                    totalBytesRead += bytesRead;
+
+                    if (totalBytesRead >= 10 * 1024)
+                    {
+                        connection.Close();
+                    }
+
+                    requestBuilder.Append(Encoding.UTF8.GetString(buffer, 0, bytesRead));
+
+                } while (networkStream.DataAvailable);
+
+                Console.WriteLine(requestBuilder);
+
+                var responseBody = "<h1>Hey from my server!</h1>";
                 var contentLength = Encoding.UTF8.GetByteCount(responseBody);
 
                 var response = @$"HTTP/1.1 200 OK    
+Server: My Web Server
+Date: {DateTime.UtcNow.ToString("r")}
 Content-Length: {contentLength}    
 Content-Type: text/html; charset=UTF-8
 
